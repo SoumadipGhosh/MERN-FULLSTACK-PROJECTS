dd42531e26c2209351d9abaf648183f51c40b28e
soumadip <soumadipghosh01@gmail.com>
2025-08-02 20:27:14 +0530

    new

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+dotenv.config();
+
+let port = process.env.PORT || 6000;
+
+let app= express();
+
+
+
+app.listen(port,()=>{
+    console.log("Hello from So2Mart Backend");
+    connectDB();
+})
