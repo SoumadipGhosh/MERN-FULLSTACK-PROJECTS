5ee7a2ba073c9311dfe217b9b5f03912d9a8e4d2
SOUVIK SAHA <souvik21102001@gmail.com>
2025-08-03 16:53:05 +0530

    index.js updated (authentication)

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+import authRouter from './routes/authRoutes.js';
 dotenv.config();
 
 let port = process.env.PORT || 5000;
 
 let app = express();
 
-app.get('/', (req, res) => {
-    res.send("Welcome to So2Mart Backend!");
-});
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
     console.log("Server is running on port ", port);
